212311b68cc6166991fbf2fa87eff6f0ed6c7389
nihatxp <nihat.aliyev889@gmail.com>
2023-05-13 11:38:30 +0300

    artisan komutu özelleştirildi ve port bilgisi .env dosyasından çekildi

---
+++ app/Console/Commands/ServeCommand.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Foundation\Console\ServeCommand as BaseServeCommand;
+
+class ServeCommand extends BaseServeCommand
+{
+    /**
+     * The current port offset.
+     *
+     * @var int
+     */
+    protected $portOffset = 0;
+
+    /**
+     * The console command name.
+     *
+     * @var string
+     */
+    protected $name = 'serve';
+
+    /**
+     * Get the host for the command.
+     *
+     * @return string
+     */
+    protected function host()
+    {
+        [$host] = $this->getHostAndPort();
+
+        return $host;
+    }
+
+    /**
+     * Get the port for the command.
+     *
+     * @return string
+     */
+    protected function port()
+    {
+        $port = $this->input->getOption('port');
+
+
+        if (is_null($port)) {
+            [, $port] = $this->getHostAndPort();
+        }
+
+        $port = env('APP_PORT', $port);
+
+        $port = $port ?: 81;
+
+        return $port + $this->portOffset;
+    }
+
+    /**
+     * Get the host and port from the host option string.
+     *
+     * @return array
+     */
+    protected function getHostAndPort()
+    {
+        $hostParts = explode(':', $this->input->getOption('host'));
+
+        return [
+            $hostParts[0],
+            $hostParts[1] ?? null,
+        ];
+    }
+}
