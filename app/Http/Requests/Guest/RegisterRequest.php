4bd9a698c9f29194f000d8d310c540aeffc9b548
nihatxp <nihat.aliyev889@gmail.com>
2023-06-04 17:47:05 +0400

    General Update

--- app/Http/Requests/Guest/RegisterRequest.php
+++ app/Http/Requests/Guest/RegisterRequest.php
@@ -44,6 +44,7 @@ class RegisterRequest extends FormRequest
             $phoneNumber = preg_replace('/(\d{2})(\d{3})(\d{3})(\d{2})(\d{2})/', '$1-$2-$3-$4-$5', $phoneNumber);
 
             $this->merge(['phone_number' => $phoneNumber]);
+            
         }
 
         return $rules;
